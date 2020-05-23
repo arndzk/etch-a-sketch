82b5fe2b5633a6a5d03488870f62771962b90a2b
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-05-23 11:34:47 +0300

    Work on flex-grid

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const grid = document.querySelector('#grid');
+const grid = document.querySelector('.flex-grid');
 
-for(let i = 0; i <= 16; i++) {
-    for(let j = 0; j <= 16; j++) {
-        const gridBox = document.createElement('div');
-        gridBox.classList.add('grid-box');
-        grid.appendChild(gridBox);
-    }
+for(let i = 0; i <= 15; i++) {
+        const gridCol = document.createElement('div');
+        gridCol.classList.add('grid-col');
+        grid.appendChild(gridCol);
 }
+
+const cols = Array.from(document.querySelectorAll('.grid-col'));
+cols.forEach((col) => {
+    for(let j = 0; j <= 15; j++) {
+        const gridSqr = document.createElement('div');
+        gridSqr.classList.add('grid-sqr');
+        col.appendChild(gridSqr);
+    }
+});
