1cf95a7af4d93e19d00b248ef82139709933731d
Arnold Dziudzik <arn.dziudzik@gmail.com>
2020-10-14 18:10:01 +0300

    milestone: initial flex-grid squares get black backgroundColor onMouseOver + flex-grid generated by function generateGrid()

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-let numOfRows = 16;
+let dimensions = 16;
 
-const grid = document.querySelector('.flex-grid');
+generateGrid(dimensions);
 
-for(let i = 0; i < numOfRows; i++) {
+
+function generateGrid(dimensions) {
+
+    const grid = document.querySelector('.flex-grid');
+
+    for(let i = 0; i < dimensions; i++) {
         const gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
         grid.appendChild(gridRow);
-}
+    }
 
-const rows = Array.from(document.querySelectorAll('.grid-row'));
-rows.forEach((row) => {
-    for(let i = 0; i < numOfRows; i++) {
+    const rows = Array.from(document.querySelectorAll('.grid-row'));
+    rows.forEach((row) => {
+    for(let i = 0; i < dimensions; i++) {
         const gridSqr = document.createElement('div');
         gridSqr.classList.add('grid-sqr');
         row.appendChild(gridSqr);
     }
-});
+    });
 
-const squares = Array.from(document.querySelectorAll('.grid-sqr'));
-squares.forEach((square) => {
-    for(let i = 0; i < (numOfRows * numOfRows); i++) {
+    const squares = Array.from(document.querySelectorAll('.grid-sqr'));
+    squares.forEach((square) => {
+        for(let i = 0; i < (dimensions * dimensions); i++) {
         square.addEventListener("mouseover", mouseOver, false);
-    }
-});
+        }
+    });
+}
 
 function mouseOver() {
-    
+    this.style.backgroundColor = "black";
 }
